afeb523636c7869e62a483d5667c0ce18450d788
Keyn <keyndark@gmail.com>
2016-01-31 23:53:43 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,343 @@
+
+"use strict";
+
+var MumbleConnection = require('./lib/MumbleConnection');
+var MumbleSocket = require('./lib/MumbleSocket');
+var tls = require('tls');
+var fs = require('fs');
+var crypto = require('crypto');
+var secret = crypto.randomBytes(24);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./mumble-server.sqlite');
+var util = require('./lib/util');
+
+function encrypt(plaintext) {
+    var cipher = crypto.createCipher('aes-128-cbc', secret);
+    cipher.setAutoPadding(false);
+    var ciphertext = '';
+    for (var i=0; i < plaintext.length; i+=16) {
+        ciphertext += cipher.update(plaintext.substr(i, i+16), 'utf8', 'base64');
+    }
+    return ciphertext.toString('base64');
+}
+
+function decrypt(ciphertext) {
+    var decipher = crypto.createDecipher('aes-128-cbc', secret);
+    decipher.setAutoPadding(false);
+    var plaintext = decipher.update(ciphertext, 'base64', 'utf8');
+    return plaintext.toString('utf8');
+}
+
+var ciphertext = encrypt(new Buffer("The secret crow ate the pie of the bear.").toString('utf8'));
+
+var options = {
+    // Chain of certificate autorities
+    // Client and server have these to authenticate keys 
+    ca: [
+          fs.readFileSync('ssl/root-cert.pem'),
+          fs.readFileSync('ssl/ca1-cert.pem'),
+          fs.readFileSync('ssl/ca2-cert.pem'),
+          fs.readFileSync('ssl/ca3-cert.pem'),
+          fs.readFileSync('ssl/ca4-cert.pem')
+        ],
+    // Private key of the server
+    key: fs.readFileSync('ssl/agent1-key.pem'),
+    // Public key of the server (certificate key)
+    cert: fs.readFileSync('ssl/agent1-cert.pem'),
+
+    // Request a certificate from a connecting client
+    requestCert: true,
+
+    // Automatically reject clients with invalide certificates.
+    rejectUnauthorized: false             // Set false to see what happens.
+};
+
+/**
+ * Encodes the version to an uint8 that can be sent to the server for version-exchange
+ **/
+function encodeVersion(major, minor, patch) {
+    return ((major & 0xffff) << 16) |  // 2 bytes major
+        ((minor & 0xff) << 8) |  // 1 byte minor
+        (patch & 0xff); // 1 byte patch
+}
+
+var users = [];
+var clients = [];
+
+// Start a TCP Server
+tls.createServer(options, function (socket) {
+    console.log("TLS Client authorized:", socket.authorized);
+    if (!socket.authorized) {
+        console.log("TLS authorization error:", socket.authorizationError);
+    }
+
+    var user;
+
+    /*console.log("Cipher: ",  socket.getCipher());
+    console.log("Address: ", socket.address());
+    console.log("Remote address: ", socket.remoteAddress);
+    console.log("Remote port: ", socket.remotePort);*/
+    //console.log("getPeerCertificate: ", socket.getPeerCertificate().fingerprint.replace(/\:/g, ''));
+
+    var connection = new MumbleConnection( socket, {});
+
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;
+    socket.mumble = connection;
+    clients.push(socket);
+
+    // Send a message to all clients
+    function broadcast(type, message, sender) {
+        clients.forEach(function (client) {
+            // Don't want to send it to sender
+            if (client === sender) {
+                return;
+            }
+
+            client.mumble.sendMessage(type, message);
+        });
+    }
+
+    socket.on('error', function(){
+        broadcast('UserRemove', {session: users[user].u.session}, socket);
+        delete users[user];
+        user = false;
+        clients.splice(clients.indexOf(socket), 1);
+        connection.disconnect();
+    });
+
+    socket.on('close', function(){
+        broadcast('UserRemove', {session: users[user].u.session}, socket);
+        delete users[user];
+        user = false;
+        clients.splice(clients.indexOf(socket), 1);
+        connection.disconnect();
+    });
+
+    connection.on('textMessage', function(m) {
+        if (m.channel_id.length) { // A message to the channel
+            var ms = {
+                actor: users[user].u.session,
+                session: [],
+                channel_id: m.channel_id,
+                tree_id: [],
+                message: m.message
+            };
+
+            users.forEach(function (row) {
+                if (m.channel_id.indexOf(row.u.channel_id) > -1 && row.u.session !== (user + 100)) {
+                    row.socket.mumble.sendMessage('TextMessage', ms);
+                }
+            });
+        }
+    });
+
+    connection.on('permissionQuery', function(m) {
+        var permissions = util.writePermissions({
+            Write: 0x01,
+            Traverse: 0x02,
+            Enter: 0x04,
+            Speak: 0x08,
+            Whisper: 0x100,
+            TextMessage: 0x200
+        });
+
+        connection.sendMessage('PermissionQuery', {permissions: permissions});
+    });
+
+    connection.on('userState', function(m) {
+        var u = {
+            actor: users[user].u.session,
+            session: users[user].u.session
+        };
+
+        if (m.actor != null) {
+            u.actor = m.actor;
+            users[user].u.actor = m.actor;
+        }
+        if (m.name != null) {
+            u.name = m.name;
+            users[user].u.name = m.name;
+        }
+        if (m.user_id != null) {
+            u.user_id = m.user_id;
+            users[user].u.user_id = m.user_id;
+        }
+        if (m.deaf != null) {
+            u.deaf = m.deaf;
+            users[user].u.deaf = m.deaf;
+        }
+        if (m.mute != null) {
+            u.mute = m.mute;
+            users[user].u.mute = m.mute;
+        }
+        if (m.recording != null) {
+            u.recording = m.recording;
+            users[user].u.recording = m.recording;
+        }
+        if (m.suppress != null) {
+            u.suppress = m.suppress;
+            users[user].u.suppress = m.suppress;
+        }
+        if (m.self_mute != null) {
+            u.self_mute = m.self_mute;
+            users[user].u.self_mute = m.self_mute;
+        }
+        if (m.self_deaf != null) {
+            u.self_deaf = m.self_deaf;
+            users[user].u.self_deaf = m.self_deaf;
+        }
+        if (m.channel_id != null) {
+            u.channel_id = m.channel_id;
+            users[user].u.channel_id = m.channel_id;
+        }
+        if (m.priority_speaker != null) {
+            u.priority_speaker = m.priority_speaker;
+            users[user].u.priority_speaker = m.priority_speaker;
+        }
+        if (m.texture_hash != null) {
+            u.texture_hash = m.texture_hash;
+            users[user].u.texture_hash = m.texture_hash;
+        }
+        if (m.comment_hash != null) {
+            u.comment_hash = m.comment_hash;
+            users[user].u.comment_hash = m.comment_hash;
+        }
+        if (m.hash != null) {
+            u.hash = m.hash;
+            users[user].u.hash = m.hash;
+        }
+        if (m.comment != null) {
+            u.comment = m.comment;
+            users[user].u.comment = m.comment;
+        }
+        if (m.plugin_identity != null) {
+            u.plugin_identity = m.plugin_identity;
+            users[user].u.plugin_identity = m.plugin_identity;
+        }
+        if (m.plugin_context != null) {
+            u.plugin_context = m.plugin_context;
+            users[user].u.plugin_context = m.plugin_context;
+        }
+        if (m.texture != null) {
+            u.texture = m.texture;
+            users[user].u.texture = m.texture;
+        }
+
+        connection.sendMessage('UserState', u);
+
+        broadcast('UserState', u, socket);
+    });
+
+    connection.on('authenticate', function(m) {
+        //connection.sendMessage('Reject', { reason: 'omg test'});
+
+        connection.sendMessage('CryptSetup', {
+        });
+
+        connection.sendMessage('SuggestConfig', {
+            version: encodeVersion(1, 2, 4)
+        });
+
+        connection.sendMessage('ServerConfig', {
+            max_bandwidth: 128000/*,
+            allow_html: true,
+            welcome_text: 'welcome_text'*/
+        });
+
+        var nuser = {u: {}};
+        var new_session = users.push(nuser);
+        new_session--;
+
+        user = new_session;
+        users[user].socket = socket;
+        users[user].u.name = m.username;
+        users[user].u.session = new_session + 100;
+        users[user].u.recording = null;
+        users[user].u.mute = null;
+        users[user].u.deaf = null;
+        users[user].u.self_mute = null;
+        users[user].u.self_deaf = null;
+        users[user].u.suppress = null;
+        users[user].u.priority_speaker = null;
+        users[user].u.hash = socket.getPeerCertificate().fingerprint.replace(/\:/g, '');
+        users[user].u.channel_id = 30;
+        connection.sendMessage('UserState', users[user].u);
+
+        broadcast('UserState', users[user].u, socket);
+
+        db.each("SELECT * FROM channels WHERE server_id=1", function(err, row) {
+            connection.sendMessage('ChannelState', {
+                channel_id: row.channel_id,
+                parent: row.parent_id,
+                name: row.name,
+                description: '',
+                temporary: false
+            });
+        }, function(err, num_row) {
+            connection.sendMessage('UserState', users[user].u);
+
+            users.forEach(function (row) {
+                if (row.u.session !== (user + 100)) {
+                    connection.sendMessage('UserState', row.u);
+                }
+            });
+        });
+
+        connection.sendMessage('CodecVersion', {
+            alpha: -2147483637,
+            beta: -2147483632,
+            opus: true
+        });
+
+        connection.sendMessage('ServerSync', {
+            session: users[user].u.session,
+            //max_bandwidth: 128000,
+            welcome_text: 'hello world'
+        });
+    });
+
+    connection.on('version', function(m) {
+        connection.sendMessage('Version', { version: encodeVersion(1, 2, 4), release: 'Node.js-client', os: 'Node.js', os_version: process.version });
+    });
+
+    connection.on('cryptSetup', function(m) {
+        console.log(m)
+    });
+
+    connection.on('userState', function(m) {
+    });
+
+    connection.on('ping', function(m) {
+        connection.sendMessage('Ping', { timestamp: m.timestamp });
+    });
+}).listen(64760);
+
+var PORT = 64760;
+var HOST = '0.0.0.0';
+
+var dgram = require('dgram');
+var server_udp = dgram.createSocket('udp4');
+
+server_udp.on('listening', function () {
+    var address = server_udp.address();
+});
+var bufferpack = require('bufferpack');
+var buffer;
+server_udp.on('message', function (message, remote) {
+    console.log(message);
+    if (message.length !== 12) {
+        return;
+    }
+
+    var q = bufferpack.unpack('>id', message, 0);
+
+    buffer = bufferpack.pack(">idiii", [0x00010204, q[1], clients.length, 5, 128000]);
+
+    server_udp.send(buffer, 0, buffer.length, remote.port, remote.address, function(err, bytes){
+        if (err){
+            throw err;
+        }
+    });
+});
+
+server_udp.bind(PORT);
