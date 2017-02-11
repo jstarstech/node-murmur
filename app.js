92347f48853e564bcedc39f68534c442bca6eab9
jstarstech <jstars.tech@gmail.com>
2017-02-12 00:56:35 +0200

    User model and event emmiter

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
 
 var MumbleConnection = require('./lib/MumbleConnection');
 var MumbleSocket = require('./lib/MumbleSocket');
-var User = require('./lib/User');
+// var User = require('./lib/User');
 var tls = require('tls');
 var fs = require('fs');
 var os = require('os');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./mumble-server.sqlite');
 var util = require('./lib/util');
+var user = require('./lib/User');
 var bufferpack = require('bufferpack');
 
 function start_server(server_id) {
-    var users = [];
     var clients = [];
     var channels = [];
     var server = {};
@@ -37,11 +37,13 @@ function start_server(server_id) {
         });
 
         if (typeof server.port == 'undefined') {
-            server.port = 64760;
+            server.port = 64761;
         }
 
         start()
     });
+
+    var muser = new user({});
 
     function start() {
         var options = {
@@ -62,72 +64,63 @@ function start_server(server_id) {
                 console.log("TLS authorization error:", socket.authorizationError);
             }
 
-            var user;
+            var uid;
             var connection = new MumbleConnection(socket, {});
 
-            socket.name = socket.remoteAddress + ":" + socket.remotePort;
-            socket.mumble = connection;
-            clients.push(socket);
-
-            // Send a message to all clients
-            function broadcast(type, message, sender) {
-                clients.forEach(function (client) {
-                    // Don't want to send it to sender
-                    if (client === sender) {
-                        return;
-                    }
-
-                    client.mumble.sendMessage(type, message);
-                });
-            }
-
-            // Send a message to all clients
-            var broadcast_audio = function (packet, source_session_id) {
-                clients.forEach(function (client) {
-                    // Don't want to send it to sender
-                    if (client.mumble.session_id === source_session_id) {
-                        return;
-                    }
-                    if (client.user.u.self_deaf) {
-                        return;
-                    }
-                    
-                    client.write(packet);
-                });
+            var boadcast_listener = function broadcast(type, message, sender_uid) {
+                if (sender_uid != uid) {
+                    connection.sendMessage(type, message);
+                }
             };
 
-            socket.on('error', function () {
-                if (typeof users[user] !== 'undefined') {
-                    broadcast('UserRemove', {session: users[user].u.session}, socket);
+            muser.on('broadcast', boadcast_listener);
+
+            var broadcast_audio = function (packet, source_session_id) {
+                // Don't want to send it to sender
+                if (muser.getUser(uid).session_id === source_session_id) {
+                    return;
                 }
-                delete users[user];
-                user = false;
-                clients.splice(clients.indexOf(socket), 1);
+                if (muser.getUser(uid).self_deaf) {
+                    return;
+                }
+
+                connection.write(packet);
+            };
+
+            muser.on('broadcast_audio', broadcast_audio);
+
+            socket.on('error', function () {
+                if (muser.getUser(uid)) {
+                    muser.emit('broadcast', 'UserRemove', {session: muser.getUser(uid).session}, uid);
+                    muser.deleteUser(uid);
+                }
+                muser.removeListener('broadcast', boadcast_listener);
+                muser.removeListener('broadcast_audio', broadcast_audio);
                 connection.disconnect();
             });
 
             socket.on('close', function () {
-                if (typeof users[user] !== 'undefined') {
-                    broadcast('UserRemove', {session: users[user].u.session}, socket);
+                if (muser.getUser(uid)) {
+                    muser.emit('broadcast', 'UserRemove', {session: muser.getUser(uid).session}, uid);
+                    muser.deleteUser(uid);
                 }
-                delete users[user];
-                user = false;
-                clients.splice(clients.indexOf(socket), 1);
+                muser.removeListener('broadcast', boadcast_listener);
+                muser.removeListener('broadcast_audio', broadcast_audio);
                 connection.disconnect();
             });
 
             connection.on('textMessage', function (m) {
                 if (m.channel_id.length) { // A message to the channel
                     var ms = {
-                        actor: users[user].u.session,
+                        actor: muser.getUser(uid).session,
                         session: [],
                         channel_id: m.channel_id,
                         tree_id: [],
                         message: m.message
                     };
 
-                    users.forEach(function (row) {
-                        if (m.channel_id.indexOf(row.u.channel_id) > -1 && row.u.session !== (user + 100)) {
+                    muser.users.forEach(function (row) {
+                        if (m.channel_id.indexOf(row.channel_id) > -1 && row.session !== muser.getUser(uid).session) {
                             row.socket.mumble.sendMessage('TextMessage', ms);
                         }
                     });
@@ -135,7 +128,6 @@ function start_server(server_id) {
             });
 
             connection.on('permissionQuery', function (m) {
-                console.log(m);
                 var permissions = util.writePermissions({
                     Write: 0x01,
                     Traverse: 0x02,
@@ -153,87 +145,10 @@ function start_server(server_id) {
             });
 
             connection.on('userState', function (m) {
-                var u = {
-                    actor: users[user].u.session,
-                    session: users[user].u.session
-                };
+                muser.updateUser(uid, m);
 
-                if (m.actor != null) {
-                    u.actor = m.actor;
-                    users[user].u.actor = m.actor;
-                }
-                if (m.name != null) {
-                    u.name = m.name;
-                    users[user].u.name = m.name;
-                }
-                if (m.user_id != null) {
-                    u.user_id = m.user_id;
-                    users[user].u.user_id = m.user_id;
-                }
-                if (m.deaf != null) {
-                    u.deaf = m.deaf;
-                    users[user].u.deaf = m.deaf;
-                }
-                if (m.mute != null) {
-                    u.mute = m.mute;
-                    users[user].u.mute = m.mute;
-                }
-                if (m.recording != null) {
-                    u.recording = m.recording;
-                    users[user].u.recording = m.recording;
-                }
-                if (m.suppress != null) {
-                    u.suppress = m.suppress;
-                    users[user].u.suppress = m.suppress;
-                }
-                if (m.self_mute != null) {
-                    u.self_mute = m.self_mute;
-                    users[user].u.self_mute = m.self_mute;
-                }
-                if (m.self_deaf != null) {
-                    u.self_deaf = m.self_deaf;
-                    users[user].u.self_deaf = m.self_deaf;
-                }
-                if (m.channel_id != null) {
-                    u.channel_id = m.channel_id;
-                    users[user].u.channel_id = m.channel_id;
-                }
-                if (m.priority_speaker != null) {
-                    u.priority_speaker = m.priority_speaker;
-                    users[user].u.priority_speaker = m.priority_speaker;
-                }
-                if (m.texture_hash != null) {
-                    u.texture_hash = m.texture_hash;
-                    users[user].u.texture_hash = m.texture_hash;
-                }
-                if (m.comment_hash != null) {
-                    u.comment_hash = m.comment_hash;
-                    users[user].u.comment_hash = m.comment_hash;
-                }
-                if (m.hash != null) {
-                    u.hash = m.hash;
-                    users[user].u.hash = m.hash;
-                }
-                if (m.comment != null) {
-                    u.comment = m.comment;
-                    users[user].u.comment = m.comment;
-                }
-                if (m.plugin_identity != null) {
-                    u.plugin_identity = m.plugin_identity;
-                    users[user].u.plugin_identity = m.plugin_identity;
-                }
-                if (m.plugin_context != null) {
-                    u.plugin_context = m.plugin_context;
-                    users[user].u.plugin_context = m.plugin_context;
-                }
-                if (m.texture != null) {
-                    u.texture = m.texture;
-                    users[user].u.texture = m.texture;
-                }
-
-                connection.sendMessage('UserState', u);
-
-                broadcast('UserState', u, socket);
+                connection.sendMessage('UserState', muser.getUser(uid));
+                muser.emit('broadcast', 'UserState', muser.getUser(uid), uid);
             });
 
             connection.sendMessage('Version', {
@@ -249,28 +164,13 @@ function start_server(server_id) {
             });
 
             connection.on('authenticate', function (m) {
-                var nuser = {u: {}};
-                var new_session = users.push(nuser);
-                new_session--;
+                var auser = {
+                    name: m.username,
+                    hash: socket.getPeerCertificate().fingerprint.replace(/\:/g, ''),
+                    channel_id: server.defaultchannel
+                };
 
-                user = new_session;
-                users[user].socket = socket;
-                users[user].u.name = m.username;
-                users[user].u.session = new_session + 100;
-                users[user].u.recording = null;
-                users[user].u.mute = null;
-                users[user].u.deaf = null;
-                users[user].u.self_mute = null;
-                users[user].u.self_deaf = null;
-                users[user].u.suppress = null;
-                users[user].u.priority_speaker = null;
-                users[user].u.hash = socket.getPeerCertificate().fingerprint.replace(/\:/g, '');
-                users[user].u.channel_id = server.defaultchannel;
-
-                connection.broadcast_audio = broadcast_audio;
-                connection.session_id = users[user].u.session;
-
-                socket.user = users[user];
+                uid = muser.addUser(auser);
 
                 //connection.sendMessage('Reject', { reason: 'omg test'});
 
@@ -304,19 +204,18 @@ function start_server(server_id) {
                         });
                     });
 
-                    connection.sendMessage('UserState', users[user].u);
+                    connection.sendMessage('UserState', muser.getUser(uid));
 
-                    users.forEach(function (row) {
-                        connection.sendMessage('UserState', row.u);
-                        if (row.u.session !== (user + 100)) {
-                            connection.sendMessage('UserState', row.u);
+                    muser.users.forEach(function (row) {
+                        if (row.session !== (muser.getUser(uid).session + 100)) {
+                            connection.sendMessage('UserState', row);
                         }
                     });
 
-                    broadcast('UserState', users[user].u, socket);
+                    muser.emit('broadcast', 'UserState', muser.getUser(uid), uid);
 
                     connection.sendMessage('ServerSync', {
-                        session: users[user].u.session,
+                        session: muser.getUser(uid).session,
                         max_bandwidth: server.bandwidth,
                         welcome_text: server.welcometext,
                         permissions: {
@@ -371,7 +270,6 @@ function start_server(server_id) {
         });
 
         server_udp.bind(server.port);
-
 
         var app = require('express')();
         var http = require('http').Server(app);
